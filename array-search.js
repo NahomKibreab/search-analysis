d4305734d7276def04e6effd71bc593e5e50ece3
Nahom Mehanzel <nknahom@gmail.com>
2021-08-14 04:50:22 +0000

    Calculating running time of Linear Search algorithm

--- array-search.js
+++ array-search.js
@@ -1,13 +1,17 @@
-// Linear Search function
 function search(array, item) {
-  let index = null;
+  let index = null; // 1
 
-  for (let i = 0; i < array.length; i++) {
+  for (
+    let i = 0; // 1
+    i < array.length; // n + 1
+    i++ // n
+  ) {
     if (item === array[i]) {
-      index = i;
+      // n
+      index = i; // only happens once if found, so not counting for worst case
       break;
     }
   }
 
-  return index;
+  return index; // 1
 }
