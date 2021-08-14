2a8dcb06a9c129ad474b167e3b623334ee6120fd
Nahom Mehanzel <nknahom@gmail.com>
2021-08-14 20:37:59 +0000

    Quadratic time algorithm

---
+++ sum.js
@@ -0,0 +1,15 @@
+// Quadratic time - to check if two numbers in the array contains the sum
+function arrayContainsSum(array, sum) {
+  for (let i = 0; i < array.length; i++) {
+    const element1 = array[i];
+
+    for (let ii = 0; ii < array.length; ii++) {
+      const element2 = array[ii];
+
+      if (element1 + element2 === sum) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
