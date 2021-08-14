ea19aef6a4d1b14dcec5aef0f81ac58af7e1041c
Nahom Mehanzel <nknahom@gmail.com>
2021-08-14 20:57:15 +0000

    Calculating running time of Linear time algorithm

--- sum.js
+++ sum.js
@@ -1,21 +1,25 @@
 // Linear time - to check if two numbers in the array contains the sum
+// Calculating running time of Linear time algorithm
 function arrayContainsSum(array, sum) {
-  let i = 0;
-  let ii = array.length - 1;
+  let i = 0; // 1
+  let ii = array.length - 1; // 1
 
   while (i <= ii) {
-    const element1 = array[i];
-    const element2 = array[ii];
-    const currentSum = element1 + element2;
+    // n + 1
+
+    const element1 = array[i]; // n
+    const element2 = array[ii]; // n
+    const currentSum = element1 + element2; // n
 
     if (currentSum === sum) {
+      // n
       return true;
     } else if (currentSum > sum) {
-      ii--;
+      // n
+      ii--; // n
     } else {
       i++;
     }
   }
-
-  return false;
+  return false; // 1
 }
