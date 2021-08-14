47ad29702b13564f2e3ec6aa7152c4b5598b6418
Nahom Mehanzel <nknahom@gmail.com>
2021-08-14 04:54:21 +0000

    Binary Search function

--- array-search.js
+++ array-search.js
@@ -1,17 +1,24 @@
 function search(array, item) {
-  let index = null; // 1
+  let min = 0;
+  let max = array.length - 1;
 
-  for (
-    let i = 0; // 1
-    i < array.length; // n + 1
-    i++ // n
-  ) {
-    if (item === array[i]) {
-      // n
-      index = i; // only happens once if found, so not counting for worst case
-      break;
+  while (true) {
+    const middleIndex = Math.floor((min + max) / 2.0);
+    const currentItem = array[middleIndex];
+
+    if (currentItem === item) {
+      // Found it
+      return middleIndex;
+    } else if (currentItem < item) {
+      // Look at the right side
+      min = middleIndex + 1;
+    } else {
+      // Look at the left side
+      max = middleIndex - 1;
+    }
+
+    if (min > max) {
+      return null;
     }
   }
-
-  return index; // 1
 }
